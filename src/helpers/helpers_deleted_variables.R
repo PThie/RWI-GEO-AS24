f2d2bb93448b0e2d3079eceaefb84153c4ddf1ec
Patrick Thiel <patrickthiel@outlook.de>
2025-02-13 14:45:52 +0100

    Create helpers_deleted_variables.R
    Function that lists the variables that should be removed for various reasons.

---
+++ src/helpers/helpers_deleted_variables.R
@@ -0,0 +1,29 @@
+helpers_deleted_variables <- function() {
+    #' @title Define variables to be removed
+    #' 
+    #' @description This function defines the variables to be removed from the
+    #' data set that are potentially in the data set because of internal process
+    #' at AutoScout24.
+    #' 
+    #' @return Vector with variables to be removed
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # define variables to be removed
+
+    cols <- c(
+        "rn", # Internally used to select most recent version of listing. no external value
+        "year", # because part of partition_date
+        "month", # because part of partition_date
+        "day", # because part of partition_date
+        "fornewmarket", # because should be always 0 since our data is only used cars
+        "currencyid", # because all prices are in Euros
+        "visibilitytypeid" # internal variable for AutoScout
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(cols)
+
+}
