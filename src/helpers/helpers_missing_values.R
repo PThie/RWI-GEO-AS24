942ad33f5116163f65eccfc3eec5382fd8cbac41
Patrick Thiel <patrickthiel@outlook.de>
2025-02-13 14:46:50 +0100

    Create helpers_missing_values.R
    Function that defines the encoding of missing data values.

---
+++ src/helpers/helpers_missing_values.R
@@ -0,0 +1,22 @@
+helpers_missing_values <- function() {
+    #' @title Define missing values
+    #' 
+    #' @description This function defines the missing values for the data set.
+    #' 
+    #' @return List of missing values
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # define missing values
+
+    missings <- list(
+        "implausible" = -5,
+        "not_specified" = -7,
+        "other" = -9
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(missings)
+}
