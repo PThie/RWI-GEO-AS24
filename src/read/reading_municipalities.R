79ca10e46448b8f1a6f3d41eda53548ed77d5b40
Patrick Thiel <patrickthiel@outlook.de>
2025-02-17 16:46:41 +0100

    Create reading_municipalities.R
    Function that reads the shapes of the municipalities in Germany.

---
+++ src/read/reading_municipalities.R
@@ -0,0 +1,28 @@
+reading_municipalities <- function(year_of_definition = NA) {
+    #' @title Read municipalities shapes
+    #' 
+    #' @description This function reads the municipalities shapes from the
+    #' Gebietseinheit folder.
+    #' 
+    #' @param year_of_definition Year of the definition of the municipalities
+    #' 
+    #' @return Spatial dataframe with municipalities shapes
+    #' @author Patrick Thiel
+
+    #--------------------------------------------------
+    # read municipalities
+
+    municipalities <- sf::st_read(
+        file.path(
+            config_paths()[["gebiete_path"]],
+            "Gemeinde",
+            year_of_definition,
+            "VG250_GEM.shp"
+        )
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(municipalities)
+}
