921f5a923b2e8f6ab585f5124614b8a661e1aa98
Patrick Thiel <patrickthiel@outlook.de>
2025-03-04 13:22:23 +0100

    Create exporting_auto_data.R
    Function to export the final dataset.

---
+++ src/export/exporting_auto_data.R
@@ -0,0 +1,52 @@
+exporting_auto_data <- function(
+    auto_data = NA
+) {
+    #' @title Exporting auto data
+    #' 
+    #' @description This function exports the cleaned and mapped auto data.
+    #' 
+    #' @param auto_data Dataframe with cleaned and mapped auto data
+    #' 
+    #' @return Dataframe with cleaned and mapped auto data
+    #' @author Patrick Thiel
+    
+    #--------------------------------------------------
+    # define name of exported dataset
+
+    suf_name <- paste0("CARMKT_", config_globals()[["next_version"]], "_SUF")
+
+    #--------------------------------------------------
+    # export data
+
+    for (file_format in config_globals()[["exported_file_formats"]]) {
+        if (file_format == "csv") {
+            write.csv(
+                auto_data,
+                file.path(
+                    config_paths()[["data_path"]],
+                    "SUF",
+                    config_globals()[["next_version"]],
+                    file_format,
+                    paste0(suf_name, ".csv")
+                ),
+                row.names = FALSE
+            )
+        } else {
+            arrow::write_parquet(
+                auto_data,
+                file.path(
+                    config_paths()[["data_path"]],
+                    "SUF",
+                    config_globals()[["next_version"]],
+                    file_format,
+                    paste0(suf_name, ".parquet")
+                )
+            )
+        }
+    }
+
+    #--------------------------------------------------
+    # return
+
+    return(auto_data)
+}
