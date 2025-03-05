a5bdc139c901fc874bb8b2ccf55f0aae297db0f1
Patrick Thiel <patrickthiel@outlook.de>
2025-03-05 11:07:21 +0100

    Create exporting_cleaned_data.R
    Function to export the cleaned wave data (delivery data) which is appended to the other waves in the next step.

---
+++ src/export/exporting_cleaned_data.R
@@ -0,0 +1,32 @@
+exporting_cleaned_data <- function(
+    auto_data = NA
+) {
+    #' @title Exporting cleaned data
+    #' 
+    #' @description This function exports the cleaned data to a parquet file.
+    #' The export is delivery specific, i.e. each delivery has its own folder.
+    #' Needed to append all data deliveries into one dataset.
+    #' 
+    #' @param auto_data Dataframe with cleaned auto data
+    #' 
+    #' @return NULL, direct export to local directory
+    #' @author Patrick Thiel
+
+    #--------------------------------------------------
+    # export data
+
+    arrow::write_parquet(
+        auto_data,
+        file.path(
+            config_paths()[["data_path"]],
+            "processed",
+            config_globals()[["current_delivery"]],
+            "clean_data.parquet"
+        )
+    )
+
+    #--------------------------------------------------
+    # return
+
+    return(NULL)
+}
