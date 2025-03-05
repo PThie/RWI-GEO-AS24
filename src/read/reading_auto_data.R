b15af51474cc915a07ff7df2b0f8a133e4261bde
Patrick Thiel <patrickthiel@outlook.de>
2025-03-05 11:48:41 +0100

    Moved time horizon info to own function
    For easier maintenance and run time improvement.

--- src/read/reading_auto_data.R
+++ src/read/reading_auto_data.R
@@ -118,57 +118,6 @@ reading_auto_data <- function(
     )
 
     #--------------------------------------------------
-    # export information on time horizon of the data
-
-    # define export path
-    directory <- file.path(
-        config_paths()[["output_path"]],
-        config_globals()[["next_version"]],
-        "info",
-        "dataset_info.txt"
-    )
-
-    # avoid appending if the file already exists
-    if (file.exists(directory)) {
-        file.remove(directory)
-    }
-
-    date_info <- as.data.frame(
-        cbind(
-            `Date labels` = c("Start date:", "End date:"),
-            Dates = c(
-                as.character(min(dta_prep$partition_date)),
-                as.character(max(dta_prep$partition_date))
-            )
-        )
-    )
-
-    # define header for export
-    header <- glue::glue(
-        "
-        #--------------------------------------------------
-        Infos for {config_globals()[['dataset_name']]} {config_globals()[['next_version']]}
-        #--------------------------------------------------
-        "
-    )
-
-    # export header
-    write(
-        header,
-        directory,
-        append = TRUE
-    )
-
-    # export dates
-    gdata::write.fwf(
-        date_info,
-        directory,
-        append = TRUE,
-        rownames = FALSE,
-        colnames = FALSE
-    )
-
-    #--------------------------------------------------
     # return
 
     return(dta_prep)
